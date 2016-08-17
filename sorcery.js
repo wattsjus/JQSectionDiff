0053f596a22f2b99c8d500e2a5c1a9df362f541a
wattsjus <wattsjus@gmail.com>
2016-08-17 16:11:07 -0500

    library used to serialize sections of the DOM

---
+++ sorcery.js
@@ -0,0 +1,68 @@
+$.fn.serializeObject = function(){
+
+	var self = this,
+		json = {},
+		push_counters = {},
+		patterns = {
+			"validate": /^[a-zA-Z][a-zA-Z0-9_\$]*(?:\[(?:\d*|[a-zA-Z0-9_\$]+)\])*$/,
+			"key":      /[a-zA-Z0-9_\$]+|(?=\[\])/g,
+			"push":     /^$/,
+			"fixed":    /^\d+$/,
+			"named":    /^[a-zA-Z0-9_\$]+$/
+		};
+
+
+	this.build = function(base, key, value){
+		base[key] = value;
+		return base;
+	};
+
+	this.push_counter = function(key){
+		if(push_counters[key] === undefined){
+			push_counters[key] = 0;
+		}
+		return push_counters[key]++;
+	};
+
+	$.each($(this).serializeArray(), function(){
+		// skip invalid keys
+		if(!patterns.validate.test(this.name)){
+			return;
+		}
+
+		var k,
+			keys = this.name.match(patterns.key),
+			merge = this.value,
+			reverse_key = this.name;
+		var persistantFields = [];
+		while((k = keys.pop()) !== undefined){
+			if($("[name=\""+k+"\"]").attr("persistant") !== undefined) {
+				persistantFields.push(k);
+			}
+			// adjust reverse_key
+			reverse_key = reverse_key.replace(new RegExp("\\[" + k + "\\]$"), '');
+
+			// push
+			if(k.match(patterns.push)){
+				merge = self.build([], self.push_counter(reverse_key), merge);
+			}
+
+			// fixed
+			else if(k.match(patterns.fixed)){
+				merge = self.build([], k, merge);
+			}
+
+			// named
+			else if(k.match(patterns.named)){
+				merge = self.build({}, k, merge);
+			}
+		}
+		if(persistantFields.length > 0) {
+			merge.persistantFields = persistantFields;
+		}
+
+		json = $.extend(true, json, merge);
+	});
+
+	return json;
+};
